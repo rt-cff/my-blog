03fef9aa9067bd39f08898f96f26753772e9f5be
Raymond Tam <raymondtamtwtp@gmail.com>
2020-09-14 00:46:00 +0800

    component(layout): update layout with bio as side bar content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Layout as AntLayout } from 'antd';
+import { Bio } from './';
+
+import 'antd/dist/antd.css';
 
 const StyledLayout = styled(AntLayout)`
   width: 60%;
   margin: 0 auto;
 `;
 
-const Layout = ({ children }) => {
+const StyledSider = styled(AntLayout.Sider)`
+  background: #fff;
+  height: 100vh;
+  padding: 30px 25px;
+`;
+
+const StyledContent = styled(AntLayout.Content)`
+  background: #fff;
+  height: 100vh;
+  padding: 30px 20px;
+`;
+
+const Layout = ({ children, showSider = true }) => {
   return (
     <StyledLayout>
-      {children}
+      {showSider && <StyledSider width={400}>
+        <Bio />
+      </StyledSider>}
+      <StyledContent>
+        {children}
+      </StyledContent>
     </StyledLayout>
   );
 };
 
 Layout.Header = AntLayout.Header;
-Layout.Sider = AntLayout.Sider;
-Layout.Content = AntLayout.Content;
+Layout.Sider = StyledSider;
+Layout.Content = StyledContent;
 Layout.Footer = AntLayout.Footer;
 
 export { Layout };
